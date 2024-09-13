30ebc01ef7be1f700cbdd0b5dfd0f17c47c9ad80
bhanu aluka <alukabhanuprakash@gmail.com>
2024-09-13 15:40:22 +0530

    responvines-Added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -101,16 +101,6 @@ const Cart = () => {
             PROCEED TO PAYMENT
           </button>
         </div>
-
-        <div className="cart-promocode">
-          <div>
-            <p>If you have a promo code, Enter it here</p>
-            <div className="cart-promocode-input">
-              <input type="text" placeholder="promo code" />
-              <button>Submit</button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
